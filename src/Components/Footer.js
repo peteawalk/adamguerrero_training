7c4b09a82bfafb0ec3fa1ebc78c4b88ac9e7eab1
peteawalk <peteawalk@gmail.com>
2020-05-04 13:29:19 -0500

    Copy edits

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; 2020 Peter Walker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; 2020 Adam Guerrero</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a><a href="https://peterwalker.dev">&amp; customized by Peter Walker </a></li>
            </ul>
 
         </div>
